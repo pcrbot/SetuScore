b2afad72ddefd11ec50f2b0f5276e18f28d63ec7
Li_Yu <619275505@qq.com>
2021-09-26 16:58:55 +0800

    start

---
+++ README.md
@@ -0,0 +1,37 @@
+# SetuScore
+一个为色图打分的插件
+
+## 使用说明
+
+在 https://cloud.baidu.com/product/imagecensoring 页面开通服务
+
+开通后进入管理台创建应用
+![img](./img/img1.png)
+
+名称和描述随便填一下就行
+![img](./img/img2.png)
+
+记住你的AppID, Api Key, Secret Key,填入`setu_score.py`里对应的位置
+![img](./img/img3.png)
+
+开通图片审核服务,有免费一万次记得领一下,用完的话就往账号里存十块钱,够用很久了
+![img](./img/img7.png)
+
+进入 https://ai.baidu.com/censoring#/strategylist 页面创建策略
+![img](./img/img4.png)
+
+这里应用选你刚才创建那个
+![img](./img/img5.png)
+
+色情拉满,然后在页面最下面保存策略
+![img](./img/img6.png)
+
+
+使用`pip install baidu-aip`指令安装SDK
+
+把文件夹放入`modules`中并在`__bot__.py`中添加该插件
+
+## 功能
+|指令|说明|
+|-----|-----|
+|打分|让机器人为图片打分,图片与指令需要在同一消息内|
---
+++ setu_score.py
@@ -0,0 +1,68 @@
+import os
+import re
+
+from nonebot import get_bot
+from aip import AipContentCensor
+from hoshino import R, Service
+from hoshino.typing import CQEvent, MessageSegment
+
+sv = Service('色图打分')
+
+cache = ''  #你go-cqhttp的位置
+APP_ID = '' #你的AppID
+API_KEY = ''#你的API Key
+SECRET_KEY = ''#你的Secret Key
+
+client = AipContentCensor(APP_ID, API_KEY, SECRET_KEY)
+
+def get_file_content(filePath):
+    with open(filePath, 'rb') as fp:
+        return fp.read()
+
+def porn_pic_index(img):
+    img = os.path.join(cache,img)
+    result = client.imageCensorUserDefined(get_file_content(img))
+    try:
+        if (result):
+            r = result
+            if "error_code" in r:
+                return { 'code': r['error_code'], 'msg': r['error_msg'] }
+            else:
+                porn = 0
+                sexy = 0
+                for c in r['data']:
+                    #由于百度的图片审核经常给出极低分,所以不合规项置信度*500后为分数
+                    if c['type'] == 1 and c['subType'] == 0:
+                        porn = int(c['probability'] * 500)
+                    elif c['type'] == 1 and c['subType'] == 1:
+                        sexy = int(c['probability'] * 500)
+                return { 'code': 0, 'msg': 'Success', 'value': max(sexy,porn) }
+
+        else:
+            return { 'code': -1, 'msg': 'API Error' }
+
+
+    except FileNotFoundError:
+        return { 'code': -1, 'msg': 'File not found' }
+
+@sv.on_prefix('打分')
+async def setu_score(bot,ev: CQEvent):
+    ret = re.search(r"\[CQ:image,file=(.*),url=(.*)\]", str(ev.message))
+    try:
+        file = ret.group(1)
+    except:
+        await bot.send(ev,'请带上图片,需要与指令在同一消息内')
+        return
+    #百度api无法直接从腾讯url获取图片,所以要下载到本地后再上传
+    img = await get_bot().get_image(file=file)
+    img_file = img['file']
+    porn = porn_pic_index(img_file)
+    if porn['code'] == 0:
+        score = porn['value']
+    else:
+        code = porn['code']
+        err = porn['msg']
+        await bot.send(ev,f'错误:{code}\n{err}')
+        return
+    url = os.path.join(cache,img_file)
+    await bot.send(ev,str(MessageSegment.image(f'file:///{os.path.abspath(url)}')+f'\n色图评分:{score}'))
